db59a8e20f39d7f55fa3fda11f1ea0b214c24e65
shelley-zeng <84942896+shelley-zeng@users.noreply.github.com>
2023-08-14 11:25:41 +1200

    Update project.py
    Version 2:
    I changed it from Version 1. I added classes and I need to redo the layout in a later version

--- project.py
+++ project.py
@@ -1,50 +1,72 @@
 """
 Filename: Shoe quiz
 Author: shelley
-Date: 25.07.23
+Date: 14.08.23
 Description: This is a quiz for the user to figure out what shoe would
 suit their lifestyle and style
-Version 1:
+Version 2:
 """
-
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
 
-window = tk.Tk()
-# this crrates sizing of the window and title
-window.geometry("300x400")
-window.title("Find your perfect shoe")
-
-# column and row configuration
-window.columnconfigure(0, weight = 5, minsize = 40)
-window.columnconfigure(1, weight = 5, minsize = 40)
-window.rowconfigure([0,1,2,3], weight = 2, minsize = 20)
-Font_tuple = ("Tahoma", 20, "bold")
-
-# question set up
-frame=tk.Frame(window, width=200, height=200, bg="#ff0")
-quiz_label = tk.Label(frame, text = "Question", font=Font_tuple)
-
-# answer buttons
-answer_1 = tk.Button(text = "Answer 1", font = Font_tuple, height = 3)
-answer_2 = tk.Button(text = "Answer 2", font = Font_tuple, height = 3)
-answer_3 = tk.Button(text = "Answer 3", font = Font_tuple, height = 3)
-answer_4 = tk.Button(text = "Answer 4", font = Font_tuple, height = 3)
-
-back_button = tk.Button (text = "<", font = Font_tuple, height = 1)
-next_button = tk.Button (text = ">", font = Font_tuple, height = 1)
+# this is just window setup
+root = tk.Tk()
+root.geometry("300x400")
+root.title("Find your perfect shoe")
 
 
-# grid layout
-quiz_label.grid(row = 0, column = 0, columnspan =2, padx=50, pady=50)
-answer_1.grid(row = 1, column = 0)
-answer_2.grid(row = 1, column = 1)
-answer_3.grid(row = 2, column = 0)
-answer_4.grid(row = 2, column = 1)
-back_button.grid(row=3, column=0)
-next_button.grid(row=3, column =1)
-frame.grid(row = 0, column = 0, columnspan =2, padx=20, pady=5)
+        
+class Window():
+    """
+    This class will be used to set up my window
+    """
 
-# makes the window not resizable
-window.resizable(False, False)
-window.mainloop()
+
+    def __init__(self, window):
+        my_frame = tk.Frame(window)
+        my_frame.pack()
+        self.welcome = tk.Label(window, text = "Welcome to this quiz. Press START to continue")
+        self.welcome.pack()
+        self.butt = tk.Button(window, text = "START", command=lambda: [self.go(window), self.butt.destroy(),self.welcome.destroy()])
+        self.butt.pack(
+)
+    def go(self,window):
+        self.q_a = {"q1":["a1","a2","a3","A4"],
+               "q2":["A1","a2","a3","a4"],
+               "q3":["a1","A2","a3","a4"],
+               "q4":["a1","a2","A3","a4"],}
+        self.question_number = 0
+        # this gets the question
+        q = list(self.q_a.keys())
+        print(q[self.question_number])
+        self.lb = tk.Label(window, text = q[self.question_number])
+        self.lb.pack()
+
+
+        # this gets the list of the list of answers
+        boop = list(self.q_a.values())
+        # this gets a list of just the answers for specified q
+        needed_boop = boop[self.question_number]
+             
+        bt_1 = tk.Button(window, text = needed_boop[0])
+        bt_1.pack()
+        btn_2 = tk.Button(window, text = needed_boop[1])
+        btn_2.pack()
+        btn_3 = tk.Button(window, text = needed_boop[2])
+        btn_3.pack()
+        btn_4 = tk.Button(window, text = needed_boop[3])
+        btn_4.pack()
+
+        bck_button = tk.Button(window, text = "<")
+        bck_button.pack()
+        nxt_button = tk.Button(window, text = ">")
+        nxt_button.pack()
+
+        numb_lbl = self.question_number + 1
+        numb_lbl = tk.Label(window, text = f"{numb_lbl}/{len(q)}")
+        numb_lbl.pack()
+
+powr_ranger = Window(root)
+
+root.mainloop()
