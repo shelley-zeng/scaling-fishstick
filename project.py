6bb76eade5138d3d3e09b3807ce8a63ab5c727f8
shelley-zeng <84942896+shelley-zeng@users.noreply.github.com>
2023-08-16 10:07:03 +1200

    Update project.py
    Version 3 - Made the next button do stuff

--- project.py
+++ project.py
@@ -1,10 +1,10 @@
 """
 Filename: Shoe quiz
 Author: shelley
-Date: 14.08.23
+Date: 16.08.23
 Description: This is a quiz for the user to figure out what shoe would
 suit their lifestyle and style
-Version 2:
+Version 3:
 """
 import tkinter as tk
 from tkinter import ttk
@@ -33,39 +33,68 @@ class Window():
 )
     def go(self,window):
         self.q_a = {"q1":["a1","a2","a3","A4"],
-               "q2":["A1","a2","a3","a4"],
-               "q3":["a1","A2","a3","a4"],
-               "q4":["a1","a2","A3","a4"],}
+               "q2":["A1","LALA2","CHERRY","RAWR"],
+               "q3":["WOOGA1","CHRISTMAS2","TWILERBEE","a4"],
+               "q4":["SOAP","SHAMPOO","CONFITIONER","TOOTHPASTE"],}
         self.question_number = 0
         # this gets the question
-        q = list(self.q_a.keys())
-        print(q[self.question_number])
-        self.lb = tk.Label(window, text = q[self.question_number])
+        self.q = list(self.q_a.keys())
+        print(self.q[self.question_number])
+        self.lb = tk.Label(window, text = self.q[self.question_number])
         self.lb.pack()
 
 
         # this gets the list of the list of answers
-        boop = list(self.q_a.values())
+        self.boop = list(self.q_a.values())
         # this gets a list of just the answers for specified q
-        needed_boop = boop[self.question_number]
+        self.needed_boop = self.boop[self.question_number]
              
-        bt_1 = tk.Button(window, text = needed_boop[0])
-        bt_1.pack()
-        btn_2 = tk.Button(window, text = needed_boop[1])
-        btn_2.pack()
-        btn_3 = tk.Button(window, text = needed_boop[2])
-        btn_3.pack()
-        btn_4 = tk.Button(window, text = needed_boop[3])
-        btn_4.pack()
+        self.btn_1 = tk.Button(window, text = self.needed_boop[0])
+        self.btn_1.pack()
+        self.btn_2 = tk.Button(window, text = self.needed_boop[1])
+        self.btn_2.pack()
+        self.btn_3 = tk.Button(window, text = self.needed_boop[2])
+        self.btn_3.pack()
+        self.btn_4 = tk.Button(window, text = self.needed_boop[3])
+        self.btn_4.pack()
 
         bck_button = tk.Button(window, text = "<")
         bck_button.pack()
-        nxt_button = tk.Button(window, text = ">")
+        nxt_button = tk.Button(window, text = ">", command = lambda: [self.next_button(window), self.lb.destroy()])
         nxt_button.pack()
 
-        numb_lbl = self.question_number + 1
-        numb_lbl = tk.Label(window, text = f"{numb_lbl}/{len(q)}")
-        numb_lbl.pack()
+        self.numb_lbl = self.question_number + 1
+        self.numb_lbl = tk.Label(window, text = f"{self.numb_lbl}/{len(self.q)}")
+        self.numb_lbl.pack()
+
+    def next_button(self, window):
+        # this lets the user go from question 1 to question 2
+        self.question_number += 1
+        self.needed_boop = self.boop[self.question_number]
+        # destroy prev. q buttons
+        self.btn_1.destroy()
+        self.btn_2.destroy()
+        self.btn_3.destroy()
+        self.btn_4.destroy()
+        self.numb_lbl.destroy()
+        
+        self.btn_1 = tk.Button(window, text= self.needed_boop[0])
+        self.btn_1.pack()
+        self.btn_2 = tk.Button(window, text = self.needed_boop[1])
+        self.btn_2.pack()
+        self.btn_3 = tk.Button(window, text = self.needed_boop[2])
+        self.btn_3.pack()
+        self.btn_4 = tk.Button(window, text = self.needed_boop[3])
+        self.btn_4.pack()
+
+        print(self.q)
+        print(self.question_number)
+        self.new_lbl = tk.Label(window, text = self.q[self.question_number])
+        self.new_lbl.pack()
+        self.numb_lbl = self.question_number + 1
+        self.numb_lbl = tk.Label(window, text = f"{self.numb_lbl}/{len(self.q)}")
+        self.numb_lbl.pack()
+
 
 powr_ranger = Window(root)
 
