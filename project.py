81ece2ebc166845aa1fd0605b92233d4e6e3fe39
shelley-zeng <84942896+shelley-zeng@users.noreply.github.com>
2023-08-29 08:52:51 +1200

    Update project.py
    Version 5 - created the layout

--- project.py
+++ project.py
@@ -1,10 +1,9 @@
 """
 Filename: Shoe quiz
 Author: shelley
-Date: 22.08.23
+Date: 24.08.23
 Description: This is a quiz for the user to figure out what shoe would
 suit their lifestyle and style
-Version 2:
 """
 import tkinter as tk
 from tkinter import ttk
@@ -15,34 +14,41 @@ import random
 root = tk.Tk()
 root.geometry("300x400")
 root.title("Find your perfect shoe")
-print(random.random())
+root.resizable(False,False)
 
         
-class Window():
+class Window:
     """
     This class will be used to set up my window
     """
 
 
     def __init__(self, window):
-        my_frame = tk.Frame(window)
-        my_frame.pack()
+        # this is the grid layout
+        window.columnconfigure([0,1,2,3,4], weight = 2)
+        window.rowconfigure([0,1,2,3], weight =5)
         self.welcome = tk.Label(window, text = "Welcome to this quiz. Press START to continue")
-        self.welcome.pack()
+
         self.butt = tk.Button(window, text = "START", command=lambda: [self.go(window), self.butt.destroy(),self.welcome.destroy()])
-        self.butt.pack(
-)
-    def go(self,window):
+        self.result_list = []
+
+        self.welcome.grid(row =0, column = 1, columnspan=4, rowspan =2) 
+        self.butt.grid(row =2, column = 2, columnspan=2)
+
         self.q_a = {"q1":["a1","a2","a3","A4"],
-               "q2":["A1","LALA2","CHERRY","RAWR"],
-               "q3":["WOOGA1","CHRISTMAS2","TWILERBEE","a4"],
-               "q4":["SOAP","SHAMPOO","CONFITIONER","TOOTHPASTE"],}
+                    "q2":["A1","LALA2","CHERRY","RAWR"],
+                    "q3":["WOOGA1","CHRISTMAS2","TWILERBEE","a4"],
+                    "q4":["SOAP","SHAMPOO","CONFITIONER","TOOTHPASTE"],}
         self.question_number = 0
+
+        
+
+    def go(self,window):
         # this gets the question
         self.q = list(self.q_a.keys())
         #print(self.q[self.question_number])
         self.lb = tk.Label(window, text = self.q[self.question_number])
-        self.lb.pack()
+        self.lb.grid(row=0, column = 1, columnspan = 4)
 
 
         # this gets the list of the list of answers
@@ -50,30 +56,29 @@ class Window():
         # this gets a list of just the answers for specified q
         self.needed_boop = self.boop[self.question_number]
              
-        self.btn_1 = tk.Button(window, text = self.needed_boop[0])
-        self.btn_1.pack()
-        self.btn_2 = tk.Button(window, text = self.needed_boop[1])
-        self.btn_2.pack()
-        self.btn_3 = tk.Button(window, text = self.needed_boop[2])
-        self.btn_3.pack()
-        self.btn_4 = tk.Button(window, text = self.needed_boop[3])
-        self.btn_4.pack()
+        self.btn_1 = tk.Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
+        self.btn_1.grid(row =1, column =1, columnspan =2)
+        self.btn_2 = tk.Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
+        self.btn_2.grid(row =1, column =3, columnspan =2)
+        self.btn_3 = tk.Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
+        self.btn_3.grid(row =2, column =1, columnspan =2)
+        self.btn_4 = tk.Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
+        self.btn_4.grid(row =2, column =3, columnspan =2)
 
         self.bck_button = tk.Button(window, text = "<")
-        self.bck_button.pack()
+        self.bck_button.grid(row =3, column =2)
         self.nxt_button = tk.Button(window, text = ">", command = lambda: [self.next_button(window)])
-        self.nxt_button.pack()
+        self.nxt_button.grid(row =3, column =3)
 
         self.numb_lbl = self.question_number + 1
         self.numb_lbl = tk.Label(window, text = f"{self.numb_lbl}/{len(self.q)}")
-        self.numb_lbl.pack()
+        self.numb_lbl.grid(row =3, column =0)
 
     def next_button(self, window):
         # this lets the user go from question 1 to question 2
         self.question_number += 1
         if self.question_number < len(self.boop) :
             #self.question_number += 1
-
             # destroy prev. q buttons
             self.btn_1.destroy()
             self.btn_2.destroy()
@@ -82,31 +87,73 @@ class Window():
             self.numb_lbl.destroy()
             self.lb.destroy()
             
-            self.btn_1 = tk.Button(window, text= self.needed_boop[0])
-            self.btn_1.pack()
-            self.btn_2 = tk.Button(window, text = self.needed_boop[1])
-            self.btn_2.pack()
-            self.btn_3 = tk.Button(window, text = self.needed_boop[2])
-            self.btn_3.pack()
-            self.btn_4 = tk.Button(window, text = self.needed_boop[3])
-            self.btn_4.pack()
+            self.btn_1 = tk.Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
+            self.btn_1.grid(row =1, column =1, columnspan =2)
+            self.btn_2 = tk.Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
+            self.btn_2.grid(row =1, column =3, columnspan =2)
+            self.btn_3 = tk.Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
+            self.btn_3.grid(row =2, column =1, columnspan =2)
+            self.btn_4 = tk.Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
+            self.btn_4.grid(row =2, column =3, columnspan =2)
 
 
             self.lb = tk.Label(window, text = self.q[self.question_number])
-            self.lb.pack()
+            self.lb.grid(row=0, column = 1, columnspan = 4)
             self.numb_lbl = self.question_number + 1
             self.numb_lbl = tk.Label(window, text = f"{self.numb_lbl}/{len(self.q)}")
-            self.numb_lbl.pack()
+            self.numb_lbl.grid(row =3, column =0)
+            
+        else:
+            # if they reach the end destroy next button. Create finish button
+            # when user clicks other stuff happens
+            self.nxt_button.destroy()
+            self.finish_btn = tk.Button(window, text = ">", command = lambda: self.the_end(window))
+            self.finish_btn.grid(row =3, column =3)
+
+    def results(self, num, the_list):
+        the_list.append(num)
+        print(the_list)
+        # if user entered btn_1 add 1
+        # if user ent
+
+    def back_button (self,window):
+        # the back button will let the user go back to the next question when clicked
+        # this lets the user go from question 1 to question 2
+        self.question_number += 1
+        if self.question_number < len(self.boop) :
+            #self.question_number += 1
+            # destroy prev. q buttons
+            self.btn_1.destroy()
+            self.btn_2.destroy()
+            self.btn_3.destroy()
+            self.btn_4.destroy()
+            self.numb_lbl.destroy()
+            self.lb.destroy()
+            
+            self.btn_1 = tk.Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
+            #self.btn_1.pack()
+            self.btn_2 = tk.Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
+            #self.btn_2.pack()
+            self.btn_3 = tk.Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
+            #self.btn_3.pack()
+            self.btn_4 = tk.Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
+            #self.btn_4.pack()
+
+
+            self.lb = tk.Label(window, text = self.q[self.question_number])
+            self.lb.grid(row=0, column = 1, columnspan = 4)
+            self.numb_lbl = self.question_number + 1
+            self.numb_lbl = tk.Label(window, text = f"{self.numb_lbl}/{len(self.q)}")
+            #self.numb_lbl.grid(row =)
 
         else:
             # if they reach the end destroy next button. Create finish button
             # when user clicks other stuff happens
             self.nxt_button.destroy()
-            self.finish_btn = tk.Button(window, text = "FINISH", command = lambda: self.the_end(window))
-            self.finish_btn.pack()
+            self.finish_btn = tk.Button(window, text = ">", command = lambda: self.the_end(window))
+            #self.finish_btn.pack()
 
-
-
+        pass
     
     def the_end(self,window):
         # when i start using grid
@@ -125,9 +172,11 @@ class Window():
 
         # add the end
         # add results
+  
+    
 
-        
+
+     
 powr_ranger = Window(root)
 
 root.mainloop()
-
