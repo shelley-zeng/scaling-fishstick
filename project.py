70c104f90dfdf7774078cc7b54d352e4855f2eb2
shelley-zeng <84942896+shelley-zeng@users.noreply.github.com>
2023-09-11 17:59:22 +1200

    Update project.py
    Version 8 - I changed the buttons and also fixed the layout a bit to look more like how it's supposed to. However, there are still some issues with how it looks with lining up which makes it not aesthetic but I can fix it in a future iteration

--- project.py
+++ project.py
@@ -9,6 +9,8 @@ import tkinter as tk
 from tkinter import ttk
 from tkinter import *
 import random
+from tkinter.ttk import Button
+
 
 # this is just window setup
 root = tk.Tk()
@@ -26,19 +28,20 @@ class Window:
         # this is the grid layout
         window.columnconfigure([0,1,2,3,4], weight = 2)
         window.rowconfigure([0,1,2,3], weight =5)
+        window.configure(bg ="#fff")
         self.welcome = tk.Label(window, text = "Welcome to this quiz. Press START to continue")
 
-        self.butt = tk.Button(window, text = "START", command=lambda: [self.go(window), self.butt.destroy(),self.welcome.destroy()])
+        self.butt = Button(window, text = "START", command=lambda: [self.go(window), self.butt.destroy(),self.welcome.destroy()])
         # this is a list of what the user clicks
         self.result_list = []
 
         self.welcome.grid(row =0, column = 1, columnspan=4, rowspan =2) 
         self.butt.grid(row =2, column = 2, columnspan=2)
 
-        self.q_a = {"What colours do you like?":["Black/White","Neutral","Pastel","Bold"],
-                    "What does a majority of your day consist of?":["Walking","Sitting","Running","Ziplining"],
-                    "What is something important when you buy shoes?":["Comfort","Easy to run","They look cool","Gives me height"],
-                    "What is your personal clothing style":["Minimalistic","Streetwear","Dark Academia","Just clothes?"],}
+        self.q_a = {"\nWhat colours do you like?":["Black/White"," Neutral","Pastel","Bold"],
+                    "What does a majority of your day \nconsist of?":["Walking","Sitting","Running","Ziplining"],
+                    "What is something important when \nyou buy shoes?":["Comfort","Easy to run","They look cool","Gives me height"],
+                    "\nWhat is your personal clothing style?":["Minimalistic","Streetwear","Dark Academia","Just clothes?"]}
         self.question_number = 0
 
         
@@ -48,8 +51,11 @@ class Window:
         self.q = list(self.q_a.keys())
         
         # this adds the question and adds it into the window
-        self.lb = tk.Label(window, text = self.q[self.question_number])
-        self.lb.grid(row=0, column = 1, columnspan = 4)
+        self.fing_frame = tk.Frame(master=window, bg = "#ECECEC", width = 200, height = 50)
+        self.lb = tk.Label(self.fing_frame, text = self.q[self.question_number],bg = "#ECECEC", fg= "#609F63")
+        self.fing_frame.grid(row=0, column = 1, columnspan = 4)
+        self.fing_frame.grid_propagate(False)
+        self.lb.grid(padx = 25)
 
 
         # this gets the list of answers and puts it in a list
@@ -58,18 +64,18 @@ class Window:
         # this gets a list of just the answers for specified q
         self.needed_boop = self.boop[self.question_number]
              
-        self.btn_1 = tk.Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
+        self.btn_1 = Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
         self.btn_1.grid(row =1, column =1, columnspan =2)
-        self.btn_2 = tk.Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
+        self.btn_2 = Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
         self.btn_2.grid(row =1, column =3, columnspan =2)
-        self.btn_3 = tk.Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
+        self.btn_3 = Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
         self.btn_3.grid(row =2, column =1, columnspan =2)
-        self.btn_4 = tk.Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
+        self.btn_4 = Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
         self.btn_4.grid(row =2, column =3, columnspan =2)
 
-        self.bck_button = tk.Button(window, text = "<", command = lambda: [self.back_button(window)])
+        self.bck_button = Button(window, text = "<", command = lambda: [self.back_button(window)],width = 3)
         self.bck_button.grid(row =3, column =2)
-        self.nxt_button = tk.Button(window, text = ">", command = lambda: [self.next_button(window)])
+        self.nxt_button = Button(window, text = ">", command = lambda: [self.next_button(window)],width = 3)
         self.nxt_button.grid(row =3, column =3)
 
         # this adds the question number into a list
@@ -93,18 +99,23 @@ class Window:
             self.numb_lbl.destroy()
             self.lb.destroy()
             
-            self.btn_1 = tk.Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
+            self.btn_1 = Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
             self.btn_1.grid(row =1, column =1, columnspan =2)
-            self.btn_2 = tk.Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
+            self.btn_2 = Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
             self.btn_2.grid(row =1, column =3, columnspan =2)
-            self.btn_3 = tk.Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
+            self.btn_3 = Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
             self.btn_3.grid(row =2, column =1, columnspan =2)
-            self.btn_4 = tk.Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
+            self.btn_4 = Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
             self.btn_4.grid(row =2, column =3, columnspan =2)
 
 
-            self.lb = tk.Label(window, text = self.q[self.question_number])
-            self.lb.grid(row=0, column = 1, columnspan = 4)
+            # this adds the question and adds it into the window
+            self.fing_frame = tk.Frame(master=window, bg = "#ECECEC", width = 200, height = 50)
+            self.lb = tk.Label(self.fing_frame, text = self.q[self.question_number],bg = "#ECECEC", fg= "#609F63")
+            self.fing_frame.grid(row=0, column = 1, columnspan = 4)
+            self.fing_frame.grid_propagate(False)
+            self.lb.grid(padx = 25)
+            
             self.numb_lbl = self.question_number + 1
             self.numb_lbl = tk.Label(window, text = f"{self.numb_lbl}/{len(self.q)}")
             self.numb_lbl.grid(row =3, column =0)
@@ -113,7 +124,7 @@ class Window:
             # if they reach the end destroy next button. Create finish button
             # when user clicks other stuff happens
             self.nxt_button.destroy()
-            self.finish_btn = tk.Button(window, text = "FINISHED BBY", command = lambda: self.the_end(window))
+            self.finish_btn = Button(window, text = ">", command = lambda: self.the_end(window), width = 3)
             self.finish_btn.grid(row =3, column =3)
 
     def results(self, num, the_list):
@@ -141,17 +152,21 @@ class Window:
             self.numb_lbl.destroy()
             self.lb.destroy()
             
-            self.btn_1 = tk.Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
+            self.btn_1 = Button(window, text = self.needed_boop[0], command = lambda:self.results(1, self.result_list))
             self.btn_1.grid(row =1, column =1, columnspan =2)
-            self.btn_2 = tk.Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
+            self.btn_2 = Button(window, text = self.needed_boop[1], command = lambda:self.results(2, self.result_list))
             self.btn_2.grid(row =1, column =3, columnspan =2)
-            self.btn_3 = tk.Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
+            self.btn_3 = Button(window, text = self.needed_boop[2], command = lambda:self.results(3, self.result_list))
             self.btn_3.grid(row =2, column =1, columnspan =2)
-            self.btn_4 = tk.Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
+            self.btn_4 = Button(window, text = self.needed_boop[3], command = lambda:self.results(4, self.result_list))
             self.btn_4.grid(row =2, column =3, columnspan =2)
 
-            self.lb = tk.Label(window, text = self.q[self.question_number])
-            self.lb.grid(row=0, column = 1, columnspan = 4)
+            # this adds the question and adds it into the window
+            self.fing_frame = tk.Frame(master=window, bg = "#ECECEC", width = 200, height = 50)
+            self.lb = tk.Label(self.fing_frame, text = self.q[self.question_number],bg = "#ECECEC", fg= "#609F63")
+            self.fing_frame.grid(row=0, column = 1, columnspan = 4)
+            self.fing_frame.grid_propagate(False)
+            self.lb.grid(padx = 25)
 
             self.q_number = self.old_q_number
             #print(type(self.numb_lbl))
