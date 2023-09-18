45f13c6a53ec6e6a8765191eebdc56e301fbc2f9
shelley-zeng <84942896+shelley-zeng@users.noreply.github.com>
2023-09-19 09:32:09 +1200

    Update project.py
    Iteration #2 - I fixed the layout of the quiz and other things in it

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ Filename: Shoe quiz.
 
 Author: shelley
 
-Date: 16.09.23
+Date: 19.09.23
 
 Description: This is a quiz for the user to figure out what shoe would
 suit their lifestyle and style.
@@ -29,44 +29,50 @@ class Window:
         and also the questions and answers for the quiz.
         """
         # this is the grid layout
-        window.columnconfigure([0, 1, 2, 3, 4], weight=2)
+        window.columnconfigure([0, 1, 2, 3, 4, 5, 6, 7], weight=2)
         window.rowconfigure([0, 1, 2, 3], weight=5)
         # background colour of window
         window.configure(bg="#fff")
 
         # start page welcome text and START button
-        welcome_text = "Welcome to this quiz. Press START to continue"
-        self.welcome = tk.Label(window, text=welcome_text)
-        self.butt = Button(window,
-                           text="START",
-                           command=lambda: [self.welcome.destroy(),
-                                            self.butt.destroy(),
+        start_font = ("Tahoma",10)
+        welcome_text = "Welcome to this quiz.\nPress START to continue"
+        self.welcome = tk.Label(window,
+                                text=welcome_text,
+                                font = start_font,
+                                bg="#FFF")
+        self.start = Button(window,
+                            text="\nSTART\n",
+                            command=lambda: [self.welcome.destroy(),
+                                            self.start.destroy(),
                                             self.next_button(window)])
 
         # grid for the start page
-        self.welcome.grid(row=0, column=1, columnspan=4, rowspan=2)
-        self.butt.grid(row=2, column=2, columnspan=2)
+        self.welcome.grid(row=0, column=1, columnspan=6, rowspan=2)
+        self.start.grid(row=2, column=2, columnspan=4)
 
         # this is a list of what the user clicks
         self.result_list = []
 
         # use \n for the button so that they aren't small
         # dictionary of all the questions and answers
-        self.q_a = {"\nWhat colours do you like?": ["Black/White", " Neutral",
-                                                    "Pastel", "Bold"],
-                    "What does most of your day\nconsist of?": ["Walking",
-                                                                "Sitting",
-                                                                "Running",
-                                                                "Ziplining"],
-                    "What is important when\nbuying shoes?": ["Comfort",
-                                                              "Easy to run",
-                                                              "They look cool",
-                                                              "Gives me height"
-                                                              ],
-                    "\nWhat is your personal clothing style?": ["Minimalistic",
-                                                                "Streetwear",
-                                                                "Academia",
-                                                                "Just clothes"
+        self.q_a = {"\nWhat colours do you like?": ["\nBlack/White\n",
+                                                    "\nNeutral\n",
+                                                    "\nPastel\n", "\nBold\n"],
+                    "What does most of your\nday consist of?": ["\nWalking\n",
+                                                                "\nSitting\n",
+                                                                "\nRunning\n",
+                                                                "\nZiplining\n"
+                                                                ],
+                    "What matters to you in\na pair of shoes?": ["\nComfort\n",
+                                                                "\nEasy to run\n",
+                                                                "\nThey're cool\n",
+                                                                "\nAdds height\n"
+                                                                ],
+                    "  What is your personal\nclothing style?": ["\nMinimalist\n",
+                                                                "\nStreetwear\n",
+                                                                "\nAcademia\n",
+                                                                "\nJust clothes\n"
                                                                 ]}
 
         # this is the starting question number
@@ -80,14 +86,18 @@ class Window:
         self.answer_list = list(self.q_a.values())
         self.users_answer_list = []
 
+        #the font tuple for the questions
+        self.font_tuple = ("Tahoma", 9, "bold")
+
+        self.user_retry = 0
+
     def next_button(self, window):
         """Return the next question."""
-        user_retry = 0
 
         # FROM 2nd q. up only if the user has entered
         # something in their previous question
         if 0 < self.q_number < len(self.answer_list):
-            if user_retry > 0:
+            if self.user_retry > 0:
                 self.btn_1.destroy()
                 self.btn_2.destroy()
                 self.btn_3.destroy()
@@ -95,7 +105,6 @@ class Window:
                 self.numb_lbl.destroy()
                 self.question_label.destroy()
                 self.question_frame.destroy()
-                self.bck_button.destroy()
                 self.nxt_button.destroy()
             else:
                 pass
@@ -110,6 +119,7 @@ class Window:
             self.question_label.destroy()
             self.question_frame.destroy()
             self.nxt_button.destroy()
+
 
             
             self.btn_1 = Button(window, text=self.the_answers[0],
@@ -131,10 +141,11 @@ class Window:
             self.question_label = tk.Label(self.question_frame,
                                            text=self.question_list[self.q_number
                                                                    ],
-                                           bg="#ECECEC", fg="#609F63")
+                                           bg="#ECECEC", fg="#609F63",
+                                           font = self.font_tuple)
             self.question_frame.grid(row=0, column=1, columnspan=4)
             self.question_frame.grid_propagate(False)
-            self.question_label.grid()
+            self.question_label.grid(padx=15, pady = 5)
 
             self.nxt_button = Button(window, text=">",
                                      command=lambda: [self.save_answer(window),
@@ -147,13 +158,14 @@ class Window:
             q_num_lbl =f"{self.q_number+1}/{len(self.question_list)}"
 
             self.numb_lbl = tk.Label(window,
-                                     text=q_num_lbl)
-            self.numb_lbl.grid(row=3, column=0)
+                                     text=q_num_lbl,
+                                     bg= "#fff")
+            self.numb_lbl.grid(row=3, column=0, sticky = "E")
 
         # this is for the first question
         elif (self.q_number < len(self.answer_list)
               and len(self.result_list) == self.q_number):
-            if user_retry > 0:
+            if self.user_retry > 0:
                 self.btn_1.destroy()
                 self.btn_2.destroy()
                 self.btn_3.destroy()
@@ -165,16 +177,17 @@ class Window:
                 self.nxt_button.destroy()
             else:
                 pass
-
+            self.user_retry += 1
             # this adds the question and adds it into the window
             self.question_frame = tk.Frame(master=window, bg="#ECECEC",
                                            width=200, height=50)
             self.question_label = tk.Label(self.question_frame,
                                            text=self.question_list[self.q_number],
-                                           bg="#ECECEC", fg="#609F63")
+                                           bg="#ECECEC", fg="#609F63",
+                                           font = self.font_tuple)
             self.question_frame.grid(row=0, column=1, columnspan=4)
             self.question_frame.grid_propagate(False)
-            self.question_label.grid(padx=25)
+            self.question_label.grid(padx=20)
 
             # this gets a list of just the answers for specified q
             self.the_answers = self.answer_list[self.q_number]
@@ -210,9 +223,9 @@ class Window:
             # this adds the question number into a list
             q_num_lbl =f"{self.q_number+1}/{len(self.question_list)}"
             self.numb_lbl = tk.Label(window,
-                                     text=q_num_lbl)
-            self.numb_lbl.grid(row=3, column=0)
-            user_retry += 1
+                                     text=q_num_lbl,
+                                     bg = "#FFF")
+            self.numb_lbl.grid(row=3, column=0, sticky = "E")
 
         elif self.q_number == len(self.answer_list):
             # destroy prev. q buttons and labels.
@@ -254,7 +267,7 @@ class Window:
         pop_up.resizable(False, False)
         error_label = tk.Label(pop_up,
                                text="Select an option before\ncontinuing")
-        error_label.grid()
+        error_label.grid(padx=15)
         close_butt = Button(pop_up, text="CLOSE",
                             command=lambda: pop_up.destroy())
         close_butt.grid()
@@ -330,16 +343,18 @@ class Window:
             self.question_label = tk.Label(self.question_frame,
                                            text=self.question_list[self.q_number
                                                                    ],
-                                           bg="#ECECEC", fg="#609F63")
+                                           bg="#ECECEC", fg="#609F63",
+                                           font = self.font_tuple)
             self.question_frame.grid(row=0, column=1, columnspan=4)
             self.question_frame.grid_propagate(False)
-            self.question_label.grid()
+            self.question_label.grid(padx=15, pady = 5)
 
             #print(type(self.numb_lbl))
             lbl_text = f"{self.q_number+1}/{len(self.question_list)}"
             self.numb_lbl = tk.Label(window,
-                                     text= lbl_text)
-            self.numb_lbl.grid(row=3, column=0)
+                                     text= lbl_text,
+                                     bg = "#fff")
+            self.numb_lbl.grid(row=3, column=0, sticky = "E")
 
             self.nxt_button = Button(window, text=">",
                                      command=lambda: [
@@ -368,16 +383,16 @@ class Window:
         # add the end
         # add results
         if sum(self.result_list) > 0 and sum(self.result_list) <= 4:
-            final_label = tk.Label(text="sneakers")
-        elif sum(self.result_list) > 4 and sum(self.result_list) <= 9:
-            final_label = tk.Label(text="Boots :P")
-        elif sum(self.result_list) > 9 and sum(self.result_list) <= 20:
-            final_label = tk.Label(text="Platform Sneakers")
+            final_label = tk.Label(text="sneakers", bg="#fff")
+        elif sum(self.result_list) > 4 and sum(self.result_list) <= 15:
+            final_label = tk.Label(text="Boots :P", bg = "#fff")
+        elif sum(self.result_list) > 15 and sum(self.result_list) <= 26:
+            final_label = tk.Label(text="Platform Sneakers", bg="#fff")
         else:
             print("-------------\nYour total is", sum(self.result_list))
-            final_label = tk.Label(text="Loafers")
+            final_label = tk.Label(text="Loafers", bg="#fff")
             print(self.result_list)
-        final_label.grid(row=1, column=1, rowspan=2, columnspan=4)
+        final_label.grid(row=1, column=1, rowspan=2, columnspan=6)
 
 open_window = Window(root)
 
